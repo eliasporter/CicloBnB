bab0748c96515b859f12294b8e13f90aea9a8dcd
Norbert_Aguilera <naguileralavalira@gmail.com>
2023-03-08 12:41:25 +0100

    Primeres Activities

--- app/src/main/java/com/example/ciclobnb/Login.java
+++ app/src/main/java/com/example/ciclobnb/Login.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
     Button inicia,pass,creaUser;
+    EditText nom,contra;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +19,8 @@ public class Login extends AppCompatActivity {
         inicia.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                nom=(EditText) findViewById(R.id.editTextTextPersonName);
+                contra=(EditText) findViewById(R.id.editTextTextPassword);
                 if(cercaUser()) {
                     Intent intent = new Intent(Login.this, PrimeraPantalla.class);
                     startActivity(intent);
@@ -25,6 +29,8 @@ public class Login extends AppCompatActivity {
         });
     }
     public boolean cercaUser(){
+        if(nom.getText().toString().equals("Admin")&&contra.getText().toString().equals("123"))
         return true;
+        return false;
     }
 }
